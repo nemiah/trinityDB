2124e4a431923e1611823ab631db54377aa5e663
Rainer Furtmeier <Rainer@Furtmeier.IT>
2012-04-25 23:19:22 +0200

    some updates

--- system/DBData/Installation.pfdb.php
+++ system/DBData/Installation.pfdb.php
@@ -1,9 +1,5 @@
 <?php echo "This is a database-file."; /*
 host&%%%&user&%%%&password&%%%&datab&%%%&httpHost
-varchar(30)&%%%&varchar(20)&%%%&varchar(20)&%%%&varchar(20)&%%%&varchar(40)
-localhost                     &%%%&open3ADev           &%%%&zjTWWa87yvfnMn2.    &%%%&open3ADev           &%%%&*                                       %%&&&
-192.168.8.243                 &%%%&open3ADev           &%%%&zjTWWa87yvfnMn2.    &%%%&open3ADev           &%%%&nix                                     %%&&&
-192.168.8.243                 &%%%&BR                  &%%%&4JG5L9zcvpZcHECj    &%%%&BR                  &%%%&nix                                     %%&&&
-localhost                     &%%%&open3ADev           &%%%&zjTWWa87yvfnMn2.    &%%%&open3ADev           &%%%&cloudData                               %%&&&
-                                                                                                                                                           
+varchar(30)&%%%&varchar(20)&%%%&varchar(20)&%%%&varchar(20)&%%%&varchar(40)                                                                                                                                                       
+localhost                     &%%%&root                &%%%&                    &%%%&phynx               &%%%&*                                       %%&&&
 */ ?>
